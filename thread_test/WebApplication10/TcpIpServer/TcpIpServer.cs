1c6653b57048280a078c7d489641ccece4c3d92f
japgo <japgo@naver.com>
2020-10-10 13:29:56 +0900

    thread test

---
+++ thread_test/WebApplication10/TcpIpServer/TcpIpServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread_test
+{
+	public class TcpIpServer
+	{
+		Thread t = new Thread( new ThreadStart( ThreadProc ) );
+
+		public TcpIpServer()
+		{
+			t.Start();
+		}
+
+		public static void ThreadProc()
+		{
+			int i = 0;
+			while( true )
+			{
+				Console.WriteLine( "ThreadProc: {0}", i++ );
+				Thread.Sleep( 1000 );
+			}
+		}
+	}
+}
